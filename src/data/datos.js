cb48beb0a639a8683bd445ad63a5ca8339807bc5
tobiasmedrano <totomedranoprog@gmail.com>
2026-09-24 17:07:50 -0300

    Inicio del Portfolio

---
+++ src/data/datos.js
@@ -0,0 +1,47 @@
+export const datos = {
+    nombre: "Tobias Medrano Tomsic",
+    edad: 24,
+    ciudad: "San Miguel de Tucumán , Argentina",
+    correo: "totomedrano25@gmail.com",
+    profesion: "Estudiante de Programación",
+    github: "https://github.com/tobiasmedrano",
+    linkedin: "https://www.linkedin.com/in/tob%C3%ADas-medrano-tomsic-495a941a6/",
+
+};
+
+export const habilidades = [
+    {id: 1, nombre: "JavaScript", nivel: 80 },
+    {id: 2, nombre: "React", nivel: 75 },
+    {id: 3, nombre: "Node.js", nivel: 70 },
+    {id: 4, nombre: "HTML y CSS", nivel: 65 }
+]
+
+export const proyectos = [
+    {
+        id: 1, 
+        nombre: "Concecionaria Autos", descripcion: "Es una plataforma digital interactiva diseñada para funcionar como un catálogo online y punto de contacto para la adquisición de vehículos. Su objetivo principal es exhibir de manera atractiva la oferta automotriz disponible, permitiendo al cliente comparar modelos, conocer especificaciones y dar el primer paso hacia la compra.",
+        tecnologias: ["HTML", "CSS"],
+        url: "concecionaria.netlify.app"
+    },
+    {
+        id: 2,
+        nombre: "Hotel Hidden",
+        descripcion: "Es una página web diseñada para un hotel ficticio, con el objetivo de proporcionar a los visitantes información detallada sobre las instalaciones, servicios y comodidades que ofrece el hotel. La página está estructurada de manera clara y atractiva, permitiendo a los usuarios explorar las diferentes secciones y obtener una visión completa de la experiencia que pueden esperar durante su estancia.",
+        tecnologias: ["HTML", "CSS"],
+        url: "https://proyectoprog2-hotel.netlify.app/"
+    },
+    {
+        id: 3,
+        nombre: "CheckPoint Blog",
+        descripcion: "Es un blog interactivo que permite a los usuarios explorar y compartir contenido relacionado con diversos temas de interés. La plataforma está diseñada para ofrecer una experiencia de navegación intuitiva, facilitando la lectura de artículos, la participación en discusiones y la interacción con otros miembros de la comunidad.", 
+        tecnologias: ["HTML", "CSS", "JavaScript"],
+        url: "https://checkpointblog.netlify.app/"
+    },
+    {
+        id: 4,
+        nombre: "HardTech ecommerce",
+        descripcion: "Es una plataforma de comercio electrónico diseñada para ofrecer una experiencia de compra intuitiva y eficiente. Permite a los usuarios navegar por una amplia gama de productos tecnológicos, realizada a modo de práctica.",
+        tecnologias: ["HTML", "CSS", "JavaScript", "React"],
+        url: "https://hard-tech.netlify.app/"
+    }
+]
